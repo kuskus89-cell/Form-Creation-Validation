5a41ac7b319d9991478f6b5824e59e59853111b9
kuskus89-cell <sundusnoor89@gmail.com>
2025-12-11 11:45:30 +0000

    trying the callbacks

---
+++ fetch-data.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetchUserData();
+});
+
+async function fetchUserData(params) {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+}
+
+const dataContainer = document.getElementById('api-data');
+
+try {
+    const response = await fetch(apiUrl);
+    const users = await response.json();
+    dataContainer.innerHTML = " ";
+ 
+const userList = document.createElement('ul');
+
+users.forEach(user => {
+    const li = document.createElement('li');
+    li.textContent = user.name;
+    userList.appendChild(li);   
+})
+
+    dataContainer.appendChild(userList);
+
+} catch (error) {
+ dataContainer.textContent = 'Faile to load user data';
+}
